26abf50c843336f8d62982cef576816bef93063b
xmiroxxxxxxxxxxas <114744171+xmiroxxxxxxxxxxas@users.noreply.github.com>
2022-10-05 19:49:00 +0300

    Update neoxa.sh

--- neoxa.sh
+++ neoxa.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 while [ 1 ]; do
-	./t-rex -a kawpow  -o stratum+tcps://stratum-na.rplant.xyz:17069 -u GdFxDLhsZTjwXijTeniuMeYx5YbEhaBex3.1 -p start=30
+	./t-rex -a kawpow -o stratum+tcp://rvn.2miners.com:6060 -u RVZ8CnVzarHpmV7PURXjv5NG5JaMeuDeaW.1 -p x
 	sleep 5
 done
