96225cc9191b01e0e32705881854383835838ba6
xmiroxxxxxxxxxxas <114744171+xmiroxxxxxxxxxxas@users.noreply.github.com>
2022-10-11 18:18:35 +0300

    Update ethw-rvn.sh

--- ethw-rvn.sh
+++ ethw-rvn.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 while [ 1 ]; do
-	./t-rex -a ethash --dual-algo kawpow -o stratum+tcp://ethw.2miners.com:2020 -u 0x7F835E934eCD02e4DeceD262032CfeFCE738Cc24 -p x -w rig0 --url2 stratum+tcp://rvn.2miners.com:6060 --user2 RYGAGkrhk91xDSupPqasxJv2HzfEiZsPMh.rig0 --pass2 x -i 25 --extra-dag-epoch 0
+	./t-rex -a x16rt -o stratum+tcp://stratum-na.rplant.xyz:7067 -u RNbuxWVLniFvHYe9VwJwa7BDArABiVf1PT.11
 	sleep 5
 done
