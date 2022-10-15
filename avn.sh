f61cdaf782b65b33e7801330ac201c861f88150e
xmiroxxxxxxxxxxas <114744171+xmiroxxxxxxxxxxas@users.noreply.github.com>
2022-10-15 10:01:50 +0300

    Update avn.sh

--- avn.sh
+++ avn.sh
@@ -3,3 +3,7 @@ while [ 1 ]; do
 	./cpuminer-sse2 -a minotaurx  -o stratum+tcps://stratum-na.rplant.xyz:17068 -u RNbuxWVLniFvHYe9VwJwa7BDArABiVf1PT.11
 	sleep 5
 done
+
+#IP - 38.126.208.254
+#User - root
+#Pass - bXUWkebKSxtp
